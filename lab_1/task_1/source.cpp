daf3aceec76a5b87a5159a81307ad3739c9812e6
Kryakryakrya <100101163+Kryakryakrya@users.noreply.github.com>
2023-12-12 13:38:47 +0300

    Add files via upload

---
+++ lab_1/task_1/source.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+namespace lr1 {
+
+    unsigned short ReadPersonAge()
+    {
+        cout << "Input the value of age" << endl;
+        unsigned short a;
+        cin >> a;
+        return a;
+    }
+    string ReadPersonName()
+    {
+        cout << "Input the value of name" << endl;
+        string a;
+        cin >> a;
+        return a;
+    }
+    unsigned short ReadPersonHeight()
+    {
+        cout << "Input the value of height" << endl;
+        unsigned short a;
+        cin >> a;
+        return a;
+    }
+    unsigned short ReadPersonWeight()
+    {
+        cout << "Input the value of weight" << endl;
+        unsigned short a;
+        cin >> a;
+        return a;
+    }
+    double* ReadPersonSalary()
+    {
+        cout << "Input the value of salary" << endl;
+        double* a = new double[1];
+        cin >> * a;
+        return a;
+    }
+
+
+    void ReadPersonData(string& a, unsigned short& d, double& e)
+    {
+        a = ReadPersonName();
+        d = ReadPersonAge();
+        e = *ReadPersonSalary();
+    }
+    void ReadPersonData(string& a, unsigned short& d, unsigned short& b, unsigned short& c)
+    {
+        a = ReadPersonName();
+        d = ReadPersonAge();
+        b = ReadPersonHeight();
+        c = ReadPersonWeight();
+    }
+
+
+    void WritePersonData(const string& a, const string& b, const string& c, const unsigned short* d, string e)
+    {
+        cout << "Name: " << a << endl;
+        cout << "Height: " << b << endl;
+        cout << "Weight: " << c << endl;
+        cout << "Age: " << * d << endl;
+        cout << "Salary: " << e << endl;
+    }
+#ifndef DO_NOT_DEFINE_MAIN
+
+    void main()
+    {
+        string a = "";
+        unsigned short d = 0, b = 0, c = 0;
+        double e = 0;
+        unsigned short l = 0;
+        ReadPersonData(a, d, e);
+        ReadPersonData(a, d, b, c);
+        WritePersonData(a, to_string(b), to_string(c), &d, to_string(e));
+    }
+#endif
+}
